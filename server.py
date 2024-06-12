bde224c5d448fabb5be9dce6aff10c726b3fbd9c
unknown <luizferbim@gmail.com>
2024-06-12 15:05:46 -0300

    teste 4

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import pandas as pd
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})  # Permitir CORS para todos os endpoints
+CORS(app, resources={r"/*": {"origins": "https://gvbim-forge-viewer-base-functions-teste.onrender.com"}})
 
 logs = []
 
